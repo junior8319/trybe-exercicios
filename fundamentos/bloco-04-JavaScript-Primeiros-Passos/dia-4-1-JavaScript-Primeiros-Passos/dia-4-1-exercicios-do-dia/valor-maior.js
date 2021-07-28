c42438e670b65880d5020e45534f206277416ba2
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-07-28 18:26:22 -0300

    Resoluçao do exercicio 2, Dia 4.1

---
+++ fundamentos/bloco-04-JavaScript-Primeiros-Passos/dia-4-1-JavaScript-Primeiros-Passos/dia-4-1-exercicios-do-dia/valor-maior.js
@@ -0,0 +1,10 @@
+let valor1 = 70;
+let valor2 = 60;
+
+if (valor1 > valor2) {
+    console.log("Maior é valor1: " + valor1);
+} else if (valor1 === valor2 ) {
+    console.log("valor1 e valor2 São estritamente iguais e valem: " + valor1);
+} else {
+    console.log("Maior é valor 2: " + valor2);
+}
