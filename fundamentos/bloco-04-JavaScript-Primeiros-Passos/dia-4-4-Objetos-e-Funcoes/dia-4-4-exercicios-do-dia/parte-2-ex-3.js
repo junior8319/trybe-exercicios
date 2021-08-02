ce6489285f22d16c6721bc68ae197bd8a473906d
Antonio Carlos <tonhaun@yahoo.com.br>
2021-08-02 11:40:41 -0300

    Resolução do exercício 3, da Parte II, Dia 4.4

---
+++ fundamentos/bloco-04-JavaScript-Primeiros-Passos/dia-4-4-Objetos-e-Funcoes/dia-4-4-exercicios-do-dia/parte-2-ex-3.js
@@ -0,0 +1,16 @@
+function findBiggerInArray (received) {
+    let array = received;
+    let control = array[0];
+    let iOfBigger = 0;
+
+    for (let i = 0; i < array.length; i += 1) {
+        if (array[i] < control) {
+            iOfBigger = i;
+            control = array[i];
+        }
+    }
+    console.log(iOfBigger);
+}
+
+findBiggerInArray([2, 3, 6, 7, 10, 1]);
+findBiggerInArray([2, 4, 6, 7, 10, 0, -3]);
