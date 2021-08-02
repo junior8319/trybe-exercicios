7015056c82dcf09bf23487cc8c6c4451a031332a
Antonio Carlos <tonhaun@yahoo.com.br>
2021-08-02 11:49:38 -0300

    Refatorada resolução  do exercício 3, da Parte II, Dia 4.4

--- fundamentos/bloco-04-JavaScript-Primeiros-Passos/dia-4-4-Objetos-e-Funcoes/dia-4-4-exercicios-do-dia/parte-2-ex-3.js
+++ fundamentos/bloco-04-JavaScript-Primeiros-Passos/dia-4-4-Objetos-e-Funcoes/dia-4-4-exercicios-do-dia/parte-2-ex-3.js
@@ -1,16 +1,16 @@
-function findBiggerInArray (received) {
+function findSmallerInArray (received) {
     let array = received;
     let control = array[0];
-    let iOfBigger = 0;
+    let iOfSmaller = 0;
 
     for (let i = 0; i < array.length; i += 1) {
         if (array[i] < control) {
-            iOfBigger = i;
+            iOfSmaller = i;
             control = array[i];
         }
     }
-    console.log(iOfBigger);
+    console.log(iOfSmaller);
 }
 
-findBiggerInArray([2, 3, 6, 7, 10, 1]);
-findBiggerInArray([2, 4, 6, 7, 10, 0, -3]);
+findSmallerInArray([2, 3, 6, 7, 10, 1]);
+findSmallerInArray([2, 4, 6, 7, 10, 0, -3]);
