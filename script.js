05828f03c47a208c0c0fa0b3af4a79f0392b3bbc
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-08-10 06:43:34 -0300

    resolvendo desafio 11, falta entender replace com exp regular

--- script.js
+++ script.js
@@ -20,16 +20,16 @@ function isValidArrayOfNumbers (receivedNumbers) {
     for (receivedNumber of receivedNumbers) {
       (receivedNumber <= 9 && receivedNumber >= 0) ? validNumbers.push(receivedNumber) : validNumbers = [];
     }
-    console.log(validNumbers);
     return validNumbers.length === 11;
   }
   
   function generatePhoneNumber (receivedNumbers) {
-    (receivedNumbers.length != 11) ? 'Array com tamanho incorreto.' : false;
-    if (!isValidArrayOfNumbers(receivedNumbers) && apparitionsAreTooMuch(receivedNumbers)) {
-        return receivedNumbers.join('');
+    if (receivedNumbers.length != 11) {
+        return  'Array com tamanho incorreto.';
+    } else if (!isValidArrayOfNumbers(receivedNumbers) || apparitionsAreTooMuch(receivedNumbers)) {
+        return 'não é possível gerar um número de telefone com esses valores';
     } else {
-        return "não é possível gerar um número de telefone com esses valores";
+        return receivedNumbers.join('').replace(/\d{11}/, (/\(\d{2}\)\s\d{5}\-\d{4}$/));
     }
   }
   
@@ -39,6 +39,7 @@ function isValidArrayOfNumbers (receivedNumbers) {
 // console.log(findIndexesOfNumbers([1,2,9,9,9,2,5,4,4,3,1]));
 // console.log(apparitionsOfNumber(9, [1,2,9,9,6,2,5,4,4,3,1]))
 // console.log(apparitionsAreTooMuch([1,2,9,9,6,2,5,4,4,3,1]))
-console.log(generatePhoneNumber([1,2,9,9,6,2,5,4,4,-3,1,1]))
 // console.log(apparitionsAreTooMuch([1,1,2,2,3,3,4,4,5,5,6]))
+console.log(generatePhoneNumber([1,2,9,9,6,2,5,4,4,-3,1]))
+console.log(generatePhoneNumber([1,2,9,9,6,2,5,4,4,3,1]))
 
