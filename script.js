7ffc5ab56cef0dd3cf0ac123888696d06d7bb1d9
Antonio Carlos <tonhaun@yahoo.com.br>
2021-08-11 10:46:54 -0300

    Atualização do arquivo script.js para resolução do desafio 13, Projeto Playground Functions

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function hydrate (receivedString) {
-    return receivedString.replace(/\D+/, ' copo(s) de água.');
+    let getNumber = '';
+    getNumber += receivedString.replace(/\D/gim, '');
+    let sumOfNumbers = 0;
+    for (let index = 0; index < getNumber.length; index += 1) {
+        sumOfNumbers += parseInt(getNumber[index]);
+    }
+    return (sumOfNumbers === 1) ? sumOfNumbers + ' copo de água' : sumOfNumbers + ' copos de água' ;
 }
+console.log(hydrate("1 cerveja, 5 cachaças"));
 console.log(hydrate("1 cerveja"));
 console.log(hydrate("1 cachaça, 5 cervejas e 1 copo de vinho"));
+console.log('5tr1ng0'.replace(/([^\d])+/gim, ''));
+console.log('1 cerveja'.replace(/\D/gim, ''));
 
