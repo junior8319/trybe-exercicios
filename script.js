b0aeddd6fe70cd5f633ac2604067201f6b69665a
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-08-11 06:37:21 -0300

    Trabalhando no Desafio 12, proj Playgroung Functions, arquivo script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-function apparitionsOfNumber (receivedNumber, receivedArray) {
-    let indexesOfRepeated = [];
-    for (let i = 0; i < receivedArray.length; i += 1) {
-        (receivedNumber === receivedArray[i]) ? indexesOfRepeated.push(i) : false
-    }
-    return indexesOfRepeated.length
+function hydrate (receivedString) {
+    return receivedString.replace(/\D+/, ' copo(s) de água.');
 }
 
-function
+console.log(hydrate("1 cerveja"));
+console.log(hydrate("1 cachaça, 5 cervejas e 1 copo de vinho"));
 
-console.log(apparitionsOfNumber(9, [1,2,9,9,6,2,5,4,4,3,1]))
