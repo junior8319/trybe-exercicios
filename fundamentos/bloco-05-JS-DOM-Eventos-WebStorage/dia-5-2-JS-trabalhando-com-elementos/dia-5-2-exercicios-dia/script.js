6e726153503f9d853f0183bf909cc73fbcbba2d1
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-08-12 15:44:16 -0300

    Resoluçao dos exercicios, tarefa 2 + adiciona id a H1 da tarefa 1

--- fundamentos/bloco-05-JS-DOM-Eventos-WebStorage/dia-5-2-JS-trabalhando-com-elementos/dia-5-2-exercicios-dia/script.js
+++ fundamentos/bloco-05-JS-DOM-Eventos-WebStorage/dia-5-2-JS-trabalhando-com-elementos/dia-5-2-exercicios-dia/script.js
@@ -1,4 +1,11 @@
 let newH1 = document.createElement('h1')
 newH1.innerText = "Exercício 5.2 - JavaScript DOM"
-
+newH1.id = "page-title"
 document.body.appendChild(newH1)
+
+let main = document.createElement('main')
+main.className = "main-content"
+
+document.body.appendChild(main)
+
+
