15b78c5a4fb18b8a88e190c25bb57201ee25ee90
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-08-12 20:11:52 -0300

    Resoluçao dos exercicios, tarefa 8

--- fundamentos/bloco-05-JS-DOM-Eventos-WebStorage/dia-5-2-JS-trabalhando-com-elementos/dia-5-2-exercicios-dia/script.js
+++ fundamentos/bloco-05-JS-DOM-Eventos-WebStorage/dia-5-2-JS-trabalhando-com-elementos/dia-5-2-exercicios-dia/script.js
@@ -18,8 +18,64 @@ sectionLeft.className = "left-content"
 
 main.appendChild(sectionLeft)
 
+let sectionRight = document.createElement('section')
+sectionRight.className = "right-content"
+
+main.appendChild(sectionRight)
+
 let newParagraph = document.createElement('p')
 newParagraph.innerText = "Algum texto"
 
 sectionCenter.appendChild(newParagraph)
 
+let newImage = document.createElement('img')
+newImage.src = "https://picsum.photos/200"
+newImage.className = "small-image"
+
+sectionLeft.appendChild(newImage)
+
+let newUl = document.createElement('ul')
+sectionRight.appendChild(newUl)
+
+let numbersList = [];
+for (let index = 0; index < 10; index += 1) {
+  switch (index) {
+    case 0:
+      numbersList.push("um")
+      break
+    case 1:
+      numbersList.push("dois")
+      break
+    case 2:
+      numbersList.push("três")
+      break
+    case 3:
+      numbersList.push("quatro")
+      break
+    case 4:
+      numbersList.push("cinco")
+      break
+    case 5:
+      numbersList.push("seis")
+      break
+    case 6:
+      numbersList.push("sete")
+      break
+    case 7:
+      numbersList.push("oito")
+      break
+    case 8:
+      numbersList.push("nove")
+      break
+    case 9:
+      numbersList.push("dez")
+      break
+  }
+}
+console.log(numbersList)
+
+for (let index = 0; index < numbersList.length; index += 1) {
+  let newLi = document.createElement('li')
+  newLi.innerText = numbersList[index]
+  newUl.appendChild(newLi)
+}
