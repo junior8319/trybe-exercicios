4505166875e5988474537c1dc7d7bb9e86f68058
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-08-19 19:29:21 -0300

    D-6.1, ex. do dia, falta dt de inicio e restante

--- fundamentos/bloco-06-HTML-e-CSS-Forms-Flexbox-e-Responsivo/dia-6-1-Formulários/dia-6-1-exercicios-dia/script.js
+++ fundamentos/bloco-06-HTML-e-CSS-Forms-Flexbox-e-Responsivo/dia-6-1-Formulários/dia-6-1-exercicios-dia/script.js
@@ -3,40 +3,13 @@
  * https://gist.github.com/kavalcante/16106796c430c1711a71c5ba1213cee5
  */
 
-const fedUnitys = [
-  { 'AC': 'Acre' },
-  { 'AL': 'Alagoas' },
-  { 'AP': 'Amapá' },
-  { 'AM': 'Amazonas' },
-  { 'BA': 'Bahia' },
-  { 'CE': 'Ceará' },
-  { 'DF': 'Distrito Federal' },
-  { 'ES': 'Espírito Santo' },
-  { 'GO': 'Goías' },
-  { 'MA': 'Maranhão' },
-  { 'MT': 'Mato Grosso' },
-  { 'MS': 'Mato Grosso do Sul' },
-  { 'MG': 'Minas Gerais' },
-  { 'PA': 'Pará' },
-  { 'PB': 'Paraíba' },
-  { 'PR': 'Paraná' },
-  { 'PE': 'Pernambuco' },
-  { 'PI': 'Piauí' },
-  { 'RJ': 'Rio de Janeiro' },
-  { 'RN': 'Rio Grande do Norte' },
-  { 'RS': 'Rio Grande do Sul' },
-  { 'RO': 'Rondônia' },
-  { 'RR': 'Roraíma' },
-  { 'SC': 'Santa Catarina' },
-  { 'SP': 'São Paulo' },
-  { 'SE': 'Sergipe' },
-  { 'TO': 'Tocantins' },
-];
-
+const fedUnitys = { 
+    initials: ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO'],
+    state:['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goías', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraíma', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
+};
 const selectStatesList = document.getElementById('federation-unitys');
-for (let index = 0; index < fedUnitys.length; index += 1) {
+for (let index = 0; index < fedUnitys.initials.length; index += 1) {
   const theUnity = document.createElement('option');
-  theUnity.innerHTML = JSON.stringify(fedUnitys[index]);
-  console.log(theUnity.value);
+  theUnity.innerText = fedUnitys.initials[index] + ' - ' + fedUnitys.state[index];
   selectStatesList.appendChild(theUnity);
 }
