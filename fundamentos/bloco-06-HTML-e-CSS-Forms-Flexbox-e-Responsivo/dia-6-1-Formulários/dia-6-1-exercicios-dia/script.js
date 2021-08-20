90d08b3b80382e50d9b7f172f63e841d24edf35c
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-08-20 14:41:17 -0300

    D-6.1, ex. do dia finalizados.

--- fundamentos/bloco-06-HTML-e-CSS-Forms-Flexbox-e-Responsivo/dia-6-1-Formulários/dia-6-1-exercicios-dia/script.js
+++ fundamentos/bloco-06-HTML-e-CSS-Forms-Flexbox-e-Responsivo/dia-6-1-Formulários/dia-6-1-exercicios-dia/script.js
@@ -1,15 +1,64 @@
-/**
- * Array com nomes dos estados brasileiros obtido em:
- * https://gist.github.com/kavalcante/16106796c430c1711a71c5ba1213cee5
- */
+window.onload = function () {
+  loadFedUnits();
+  submitResumee();
+  isValidDate();
+}
 
-const fedUnitys = { 
-    initials: ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO'],
-    state:['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goías', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraíma', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
-};
-const selectStatesList = document.getElementById('federation-unitys');
-for (let index = 0; index < fedUnitys.initials.length; index += 1) {
-  const theUnity = document.createElement('option');
-  theUnity.innerText = fedUnitys.initials[index] + ' - ' + fedUnitys.state[index];
-  selectStatesList.appendChild(theUnity);
+function loadFedUnits () {
+  /**
+   * Array com nomes dos estados brasileiros obtido em:
+   * https://gist.github.com/kavalcante/16106796c430c1711a71c5ba1213cee5
+   */
+  const fedUnits = { 
+      initials: ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO'],
+      state:['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goías', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraíma', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
+  };
+  const selectStatesList = document.getElementById('federation-units');
+  for (let index = 0; index < fedUnits.initials.length; index += 1) {
+    const theUnity = document.createElement('option');
+    theUnity.innerText = fedUnits.initials[index] + ' - ' + fedUnits.state[index];
+    selectStatesList.appendChild(theUnity);
+  }
+}
+
+
+function submitResumee () {
+  const buttonSubmit = document.getElementById('btn-submit');
+  
+  buttonSubmit.addEventListener('click', (receivedEvent) => {
+    // receivedEvent.preventDefault();
+    console.log('Dados enviados.');
+  });
+}
+
+function isValidDate () {
+  const receivedInput = document.getElementById('input-init-date');
+  const buttonSubmit = document.getElementById('btn-submit');
+
+  buttonSubmit.addEventListener('click', () => {
+    if (receivedInput.value.length === 0) {
+      window.alert('É necessário informar uma data.');
+    } else {
+      console.log(receivedInput.value);
+      const splitedDate = splitDate(receivedInput.value);
+      if (!(splitedDate.day > 0 && splitedDate.day <= 31)) {
+        window.alert('Dia precisa estar entre 1 e 31.')
+      }
+      if (!(splitedDate.month > 0 && splitedDate.month <=12)) {
+        window.alert('Mês precisa estar em um intervalo entre 1 e 12.');
+      }
+      if (!(splitedDate.year >0)) {
+        window.alert('Ano precisar ser um número maior que 0.')
+      }
+    }
+    
+  });
+}
+
+function splitDate (date) {
+  const splitedDate = date.split('/');
+  const day = splitedDate[0];
+  const month = splitedDate[1];
+  const year = splitedDate[2];
+  return {day, month, year};
 }
