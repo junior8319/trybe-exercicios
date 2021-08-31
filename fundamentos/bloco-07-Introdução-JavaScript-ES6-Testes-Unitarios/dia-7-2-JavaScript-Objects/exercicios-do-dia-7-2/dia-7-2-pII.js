fe6f1688bdd8fbae3ede39cb4882e8a82b20fa90
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-08-31 19:22:54 -0300

    Exercicios do dia 7.2, Parte II, finalizados

--- fundamentos/bloco-07-Introdução-JavaScript-ES6-Testes-Unitarios/dia-7-2-JavaScript-Objects/exercicios-do-dia-7-2/dia-7-2-pII.js
+++ fundamentos/bloco-07-Introdução-JavaScript-ES6-Testes-Unitarios/dia-7-2-JavaScript-Objects/exercicios-do-dia-7-2/dia-7-2-pII.js
@@ -49,5 +49,41 @@ numberOfStudentsInLessons();
 
 const getValueByNumber = (object, number) => Object.values(object)[number];
 const getKeyByNumber = (object, number) => Object.keys(object)[number];
-console.log(getKeyByNumber(lesson2, 2));
-console.log(getValueByNumber(lesson2, 2));
+// console.log(getKeyByNumber(lesson2, 2));
+// console.log(getValueByNumber(lesson2, 2));
+
+const isPair = (object, key, value) => {
+  const objectArray = Object.entries(object);
+  let isEqual = false;
+  for (let index in objectArray) {
+    if (objectArray[index][0] === key && objectArray[index][1] === value) isEqual = true;
+  }
+  return isEqual;
+}
+// console.log(isPair(lesson1, 'materia', 'Inglês'));
+
+const studentsInLesson = (subject) => {
+  const lessonsInAllLessons = Object.assign({}, Object.values(allLessons));
+  let sumOfStudents = 0;
+  for (const index in lessonsInAllLessons) {
+    if (lessonsInAllLessons[index].materia === subject) sumOfStudents += parseInt(lessonsInAllLessons[index].numeroEstudantes);
+  }
+  return sumOfStudents;
+}
+// console.log(studentsInLesson('Matemática'));
+// console.log(studentsInLesson('História'));
+
+const createReport = (object, teacher) => {
+  const teacherInAllLessons = Object.assign({}, Object.values(object));
+  let sumOfPupils = 0;
+  let theLessonsOfThis = [];
+  for (let index in teacherInAllLessons) {
+    if(teacherInAllLessons[index].professor === teacher) {
+      sumOfPupils += parseInt(teacherInAllLessons[index].numeroEstudantes);
+      theLessonsOfThis.push(teacherInAllLessons[index].materia);
+    }
+  }
+  return {professor: teacher, aulas: theLessonsOfThis, estudantes: sumOfPupils };
+}
+
+console.log(createReport(allLessons, 'Carlos'));
