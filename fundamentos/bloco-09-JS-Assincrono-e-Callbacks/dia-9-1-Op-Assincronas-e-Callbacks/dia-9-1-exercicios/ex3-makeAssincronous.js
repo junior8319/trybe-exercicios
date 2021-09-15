70ce6e99b5b724a4502923ffd94551d1fd20b165
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-09-15 19:27:48 -0300

    Dia 9.1, exercicio 3

---
+++ fundamentos/bloco-09-JS-Assincrono-e-Callbacks/dia-9-1-Op-Assincronas-e-Callbacks/dia-9-1-exercicios/ex3-makeAssincronous.js
@@ -0,0 +1,13 @@
+const getPlanet = () => {
+  const mars = {
+    name: "Mars",
+    distanceFromSun: {
+      value: 227900000,
+      measurementUnit: "kilometers",
+    },
+  };
+  setTimeout(() => console.log("Returned planet: ", mars), 4000);
+};
+
+getPlanet(); // imprime Marte depois de 4 segundos
+// setTimeout(() => getPlanet(), 4000); // primeira forma que resolvi, mas o Course queria da forma como deixei(acima).
