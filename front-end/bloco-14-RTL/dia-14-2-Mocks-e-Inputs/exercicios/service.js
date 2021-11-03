5b6fd36a68991e4372cdb61c095e007262eb8c97
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-11-03 19:23:37 -0300

    Dia 14.2 - exercicio 4

--- front-end/bloco-14-RTL/dia-14-2-Mocks-e-Inputs/exercicios/service.js
+++ front-end/bloco-14-RTL/dia-14-2-Mocks-e-Inputs/exercicios/service.js
@@ -1,4 +1,15 @@
 const genRandomNumber = () => Math.floor(Math.random() * 100);
 genRandomNumber();
 
-module.exports = { genRandomNumber };
+const transformToUpperCase = (receivedString) => receivedString.toUpperCase();
+
+const returnFirstLetter = (receivedString) => receivedString.slice(0, 1);
+
+const concatStrings = (stringA, stringB) => stringA.concat(stringB);
+
+module.exports = {
+  genRandomNumber,
+  transformToUpperCase,
+  returnFirstLetter,
+  concatStrings,
+};
