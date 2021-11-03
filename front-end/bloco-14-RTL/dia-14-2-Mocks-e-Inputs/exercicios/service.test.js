2628d4b519bed471d325c80f7d7727f0c621e592
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-11-03 18:25:19 -0300

    Dia 14.2 - exercicio 3

--- front-end/bloco-14-RTL/dia-14-2-Mocks-e-Inputs/exercicios/service.test.js
+++ front-end/bloco-14-RTL/dia-14-2-Mocks-e-Inputs/exercicios/service.test.js
@@ -18,3 +18,15 @@ test('Simula que a função agora recebe 2 número e calcula a divisão', () => 
   expect(genRandomNumber).toBeCalled();
   expect(genRandomNumber).toBeCalledTimes(1);
 });
+
+describe('Teste(s) do exercício 3', () => {
+  it('Testa a função com 3 parâmetros, retorna a multiplicação e mais', () => {
+    const numberA = 20;
+    const numberB = 5;
+    const numberC = 18;
+    genRandomNumber = jest.fn().mockImplementation((a, b, c) => a * b * c);
+    expect(genRandomNumber(numberA, numberB, numberC)).toBe(1800);
+    expect(genRandomNumber).toBeCalled();
+    expect(genRandomNumber).toBeCalledTimes(1);
+  });
+});
