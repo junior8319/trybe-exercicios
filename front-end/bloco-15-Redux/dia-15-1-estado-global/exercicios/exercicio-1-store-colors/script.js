ff77a7e8bb61af061b0b87e1136e424f8d9c2443
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-11-08 17:43:37 -0300

    Dia 15.1, exercicio 1

--- front-end/bloco-15-Redux/dia-15-1-estado-global/exercicios/exercicio-1-store-colors/script.js
+++ front-end/bloco-15-Redux/dia-15-1-estado-global/exercicios/exercicio-1-store-colors/script.js
@@ -19,6 +19,13 @@ const reducer = (state = ESTADO_INICIAL, action) => {
           state.index - 1,
       };
 
+    case 'RANDOM_COLOR':
+      return {
+        ...state,
+        colors: [...state.colors, generateRandomColor()],
+        index: state.colors.length,
+      }
+
     default:
       return state;
   }
@@ -34,6 +41,22 @@ document.getElementById('next').addEventListener('click', () => {
 document.getElementById('previous').addEventListener('click', () => {
   store.dispatch({ type: 'PREVIOUS_COLOR' });
   console.log('previous');
+});
+
+const generateRandomColor = () => {
+  const eachCharacter = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+  let color = '#';
+  const random = () => Math.floor(Math.random() * eachCharacter.length);
+  for (let index = 0; index < 6; index += 1) {
+    color += eachCharacter[random()];
+  }
+  console.log(color);
+  return color;
+};
+
+document.getElementById('random').addEventListener('click', () => {
+  store.dispatch({ type: 'RANDOM_COLOR' });
+  console.log('random');
 });
 
 store.subscribe(() => {
