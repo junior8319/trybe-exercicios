1786868b7bc3279c33c37e1e3f4855a5f18f2bb5
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-12-02 19:20:30 -0300

    Dia 17.2 - exemplo de hooks

---
+++ front-end/bloco-17-Context-e-Hooks/dia-17-2-hooks/exercicios/d-17-2-hooks-praticar/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import ComponenteX from './pages/ComponenteX';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          React com Context
+        </p>
+      </header>
+      <section>
+        <ComponenteX />
+      </section>
+    </div>
+  );
+}
+
+export default App;
