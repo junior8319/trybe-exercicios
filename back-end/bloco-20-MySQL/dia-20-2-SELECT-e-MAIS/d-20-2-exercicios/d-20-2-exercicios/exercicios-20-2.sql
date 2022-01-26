f03239a01810514cf2ad2f6aae955a50d035d6c3
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-01-26 05:07:03 -0300

    queries sql on mysql Workbench - exercises of the day 20.2

---
+++ back-end/bloco-20-MySQL/dia-20-2-SELECT-e-MAIS/d-20-2-exercicios/d-20-2-exercicios/exercicios-20-2.sql
@@ -0,0 +1,27 @@
+USE Scientists;
+SELECT 'This is SQL Exercise, Practice and Solution';
+SELECT	01 as number_one,
+		02 as number_two,
+        03 as number_three;
+SELECT 10 + 15;
+SELECT (20 * 8) / (8 * 20);
+SELECT * FROM Scientists;
+SELECT	Name AS 'Nome do Projeto',
+		Hours AS 'Tempo de Trabalho'
+        FROM Projects;
+SELECT * FROM Scientists ORDER BY Name; /* Crescente ou Decrescente? Deixei crescente*/
+SELECT * FROM Projects ORDER BY Name DESC;
+SELECT CONCAT(
+	'O projeto ',
+	Name,
+	' precisou de ',
+	Hours,
+	' horas para ser concluído')
+    AS resultado
+	FROM Projects;
+SELECT Name, Hours FROM Projects ORDER BY Hours DESC;
+SELECT DISTINCT Project FROM AssignedTo;
+SELECT Name, Hours FROM Projects ORDER BY Hours DESC LIMIT 1;
+SELECT Name, Hours FROM Projects ORDER BY Hours LIMIT 1 OFFSET 1;
+SELECT * FROM Projects ORDER BY Hours LIMIT 5;
+SELECT CONCAT("Existem ", COUNT(DISTINCT Name), " cientistas na tabela Scientists") FROM Scientists;
