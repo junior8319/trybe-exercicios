4822da9dabaf0bff2a5c868f671a072acc0ab9a8
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-02-12 09:53:04 -0300

    day 22.1 - fix: moving files to fix block 22 folder organization.

--- back-end/bloco-22-NodeJS/dia-22-1-NodeJS-Introducao/dia-22-1-exercicios/d-22-1-sortition/genRandomNumber.js
+++ back-end/bloco-22-NodeJS/dia-22-1-NodeJS-Introducao/dia-22-1-exercicios/d-22-1-sortition/genRandomNumber.js
@@ -1,4 +1,4 @@
-const numberChosen = require('./sortition');
+const numberChosen = require('../sortition');
 
 const randomNumber = (Math.random() * (10 - 0) + 0).toFixed(0);
 console.log(`Número aleatório gerado ${randomNumber}`);
