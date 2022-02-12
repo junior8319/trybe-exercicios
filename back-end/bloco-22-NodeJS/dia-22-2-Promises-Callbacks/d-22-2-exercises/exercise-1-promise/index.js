5ffa60755733e943d3733e04382fbc5cdeef8490
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-02-12 17:57:43 -0300

    day 22.2 - exercise 2: call the Promise function with random numbers.

--- back-end/bloco-22-NodeJS/dia-22-2-Promises-Callbacks/d-22-2-exercises/exercise-1-promise/index.js
+++ back-end/bloco-22-NodeJS/dia-22-2-Promises-Callbacks/d-22-2-exercises/exercise-1-promise/index.js
@@ -4,24 +4,46 @@ function receiveNumbers(num1, num2, num3) {
       return reject(new Error("Informe apenas números."));
     }
 
-    const resultado = (num1 + num2) * num3;
-    return resolve(resultado);
+    const result = (num1 + num2) * num3;
+
+    if (result < 50) {
+      return reject(new Error('Valor muito baixo'));
+    }
+
+    return resolve(result);
   });
 }
 
 receiveNumbers('a', 1, 2)
   .then(result => console.log(`sucesso: ${result}`))
-  .catch(err => console.log(`erro: ${err}`));
+  .catch(err => console.log(`${err}`));
 
 receiveNumbers(3, 1, 2)
+  .then(result => console.log(`sucesso: ${result}`))
+  .catch(err => console.log(`${err}`));
+
+  receiveNumbers(3, 5, 10)
   .then(result => console.log(`sucesso: ${result}`))
   .catch(err => console.log(`erro: ${err}`));
 
 const main = async (n1, n2, n3) => {
   try {
-    await receiveNumbers(n1, n2, n3);
+    const result = await receiveNumbers(n1, n2, n3);
+    console.log(result);
+    return result;
   } catch (error) {
     console.log(error);
   }
 };
-// i have to fix the main() function.
+
+
+const generateRandomNumber = () => Math.floor(Math.random() *100 + 1);
+
+const callMainWithRandoms = () => {
+  const randomNumbers = Array.from({ length: 3 }).map(generateRandomNumber);
+  main(...randomNumbers)
+    .then(result => console.log(result))
+    .catch(err => console.log(err.message));
+};
+
+callMainWithRandoms();
