6474d20ab09205c5e7b0599c3015325860da6a89
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-02-20 15:10:48 -0300

    day 23.1 - bomb commit: examples and fixation exercises from day 23.1 and beginning of day 23.2.

--- back-end/bloco-23-Arquitetura/dia-23-1-Models-e-connection/d-23-1-exemplos/index.js
+++ back-end/bloco-23-Arquitetura/dia-23-1-Models-e-connection/d-23-1-exemplos/index.js
@@ -3,13 +3,47 @@
 const express = require('express');
 
 const Author = require('./models/Author');
+const Book = require('./models/Book');
 
 const app = express();
 
 app.get('/authors', async (_req, res) => {
   const authors = await Author.getAll();
 
-  res.status(200).json(authors);
+  return res.status(200).json(authors);
+});
+
+app.get('/authors/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const author = await Author.findById(id);
+
+  if (!author) return res.status(404).json({ message: 'Not found' });
+
+  res.status(200).json(author);
+});
+
+app.get('/books', async (req, res) => {
+  const { author_id } = req.query;
+  console.log(req.query);
+  const books = (author_id)
+  ? await Book.getByAuthorId(Number(author_id))
+  : await Book.getAll();
+  
+  return res.status(200).json(books);
+});
+
+app.get('/books', async (_req, res) => {
+  const books = await Book.getAll();
+
+  return res.status(200).json(books);
+});
+
+app.get('/books/:id', async (req, res) => {
+  const { id } = req.params;
+  const book = await Book.findBookById(id);
+
+  return res.status(200).json(book);
 });
 
 const PORT = process.env.PORT || 3000;
