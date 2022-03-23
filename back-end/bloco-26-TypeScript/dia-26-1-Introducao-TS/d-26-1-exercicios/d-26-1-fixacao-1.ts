c904ec8b47129be8f794490482f98ecb2db0a8e1
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-03-23 18:27:30 -0300

    dia 26.1: exercício de fixação 1.

---
+++ back-end/bloco-26-TypeScript/dia-26-1-Introducao-TS/d-26-1-exercicios/d-26-1-fixacao-1.ts
@@ -0,0 +1,41 @@
+enum directionsGamePad {
+  UP = "UP",
+  DOWN = "DOWN",
+  LEFT = "LEFT",
+  RIGHT = "RIGHT"
+}
+
+enum weekDays {
+  SUNDAY = 1,
+  MONDAY,
+  TUESDAY,
+  WEDNESDAY,
+  THURSDAY,
+  FRIDAY,
+  SATURDAY
+}
+
+enum rainbowColors {
+  RED = "Vermelho",
+  ORANGE = "Laranja",
+  YELLOW = "Amarelo",
+  GREEN = "Verde",
+  BLUE = "Azul",
+  INDIGO = "Anil",
+  VIOLET = "Violeta"
+}
+
+enum actions {
+  SAVE,
+  PRINT,
+  OPEN,
+  VIEW,
+  CLOSE
+}
+
+enum cardinalPoints {
+  NORTH = "N",
+  SOUTH = "S",
+  EAST = "E",
+  WESR = "W"
+}
