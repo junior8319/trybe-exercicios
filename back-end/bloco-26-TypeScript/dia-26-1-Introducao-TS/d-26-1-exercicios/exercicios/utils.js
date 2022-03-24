c6e6d45a20eba6e482f2670a3dde1e94f061e550
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-03-24 10:35:16 -0300

    dia 26.1 - exercício 7: implementa arquivo utils, que contém as funções para conversão de unidades e de tratamento de erros.

---
+++ back-end/bloco-26-TypeScript/dia-26-1-Introducao-TS/d-26-1-exercicios/exercicios/utils.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.convert = void 0;
+const throwError = (unity) => {
+    throw new Error(`A unidade ${unity} não é uma unidade válida`);
+};
+const convert = (value, baseUnity, convertUnity, units) => {
+    if (!units.includes(baseUnity))
+        throwError(baseUnity);
+    if (!units.includes(convertUnity))
+        throwError(convertUnity);
+    const indexBaseUnity = units.indexOf(baseUnity);
+    const indexConvertUnity = units.indexOf(convertUnity);
+    const exponent = indexConvertUnity - indexBaseUnity;
+    const convertedUnity = value * Math.pow(10, exponent);
+    return `A conversão de ${value}${baseUnity} para ${convertUnity} resulta em ${convertedUnity}${convertUnity}`;
+};
+exports.convert = convert;
---
+++ back-end/bloco-26-TypeScript/dia-26-1-Introducao-TS/d-26-1-exercicios/exercicios/utils.ts
@@ -0,0 +1,17 @@
+const throwError = (unity: string) => {
+  throw new Error(`A unidade ${unity} não é uma unidade válida`);
+}
+
+export const convert = (value: number, baseUnity: string, convertUnity: string, units: string[]) => {
+  if(!units.includes(baseUnity)) throwError(baseUnity);
+  if(!units.includes(convertUnity)) throwError(convertUnity);
+
+  const indexBaseUnity = units.indexOf(baseUnity);
+  const indexConvertUnity = units.indexOf(convertUnity);
+
+  const exponent = indexConvertUnity - indexBaseUnity;
+
+  const convertedUnity = value * Math.pow(10, exponent);
+
+  return `A conversão de ${value}${baseUnity} para ${convertUnity} resulta em ${convertedUnity}${convertUnity}`;
+}
