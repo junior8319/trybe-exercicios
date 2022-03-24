61ceee4ddde1786805861392e4bb75c1f4d1c301
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-03-24 09:44:20 -0300

    dia 26.1 - exercícios: índice para as chamadas às funções dos exercícios.

---
+++ back-end/bloco-26-TypeScript/dia-26-1-Introducao-TS/d-26-1-exercicios/exercicios/index.js
@@ -0,0 +1,44 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Length = __importStar(require("./length"));
+const Mass = __importStar(require("./mass"));
+const Volume = __importStar(require("./capacity"));
+console.log(Length.convert(15, 'km', 'dm'));
+console.log(Length.convert(15, 'dm', 'km'));
+console.log(Length.convert(15, 'm', 'hm'));
+console.log(Length.convert(15, 'm', 'mm'));
+console.log(Length.convert(15, 'km', 'mm'));
+console.log(Mass.massConvert(150, 'g', 'kg'));
+console.log(Mass.massConvert(150, 'cg', 'kg'));
+console.log(Mass.massConvert(150, 'kg', 'g'));
+console.log(Volume.volumeConvert(200, 'ml', 'kl'));
+console.log(Volume.volumeConvert(200, 'cl', 'hl'));
+console.log(Volume.volumeConvert(200, 'dl', 'dal'));
+console.log(Volume.volumeConvert(200, 'l', 'l'));
+console.log(Volume.volumeConvert(200, 'dal', 'dl'));
+console.log(Volume.volumeConvert(200, 'hl', 'cl'));
+console.log(Volume.volumeConvert(200, 'kl', 'ml'));
+console.log(Mass.massConvert(150, 'kg', 'fg'));
---
+++ back-end/bloco-26-TypeScript/dia-26-1-Introducao-TS/d-26-1-exercicios/exercicios/index.ts
@@ -0,0 +1,23 @@
+import * as Length from "./length";
+import * as Mass from './mass';
+import * as Volume from './capacity';
+
+console.log(Length.convert(15, 'km', 'dm'));
+console.log(Length.convert(15, 'dm', 'km'));
+console.log(Length.convert(15, 'm', 'hm'));
+console.log(Length.convert(15, 'm', 'mm'));
+console.log(Length.convert(15, 'km', 'mm'));
+
+console.log(Mass.massConvert(150, 'g', 'kg'));
+console.log(Mass.massConvert(150, 'cg', 'kg'));
+console.log(Mass.massConvert(150, 'kg', 'g'));
+
+console.log(Volume.volumeConvert(200, 'ml', 'kl'));
+console.log(Volume.volumeConvert(200, 'cl', 'hl'));
+console.log(Volume.volumeConvert(200, 'dl', 'dal'));
+console.log(Volume.volumeConvert(200, 'l', 'l'));
+console.log(Volume.volumeConvert(200, 'dal', 'dl'));
+console.log(Volume.volumeConvert(200, 'hl', 'cl'));
+console.log(Volume.volumeConvert(200, 'kl', 'ml'));
+
+console.log(Mass.massConvert(150, 'kg', 'fg'));
