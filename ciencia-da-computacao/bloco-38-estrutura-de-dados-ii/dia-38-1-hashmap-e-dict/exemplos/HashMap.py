494b2a5a2cfcb47690650f8bdfa2e2a9a493bdf2
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-08-12 14:37:59 -0300

    dia 38.1 - exemplos

--- ciencia-da-computacao/bloco-38-estrutura-de-dados-ii/dia-38-1-hashmap-e-dict/exemplos/HashMap.py
+++ ciencia-da-computacao/bloco-38-estrutura-de-dados-ii/dia-38-1-hashmap-e-dict/exemplos/HashMap.py
@@ -5,10 +5,8 @@ class HashMap:
             for index in range(10)
         ]
 
-
     def get_address(self, id_num):
         return id_num % 10
-
 
     def insert(self, employee):
         address = self.get_address(employee.id_num)
@@ -20,7 +18,7 @@ class HashMap:
             if item.id_num == id_num:
                 return item.name
         return None
-    
+
     def get_bucket_index(self, id_num):
         if self.has_this_address(id_num):
             address = self.get_address(id_num)
@@ -35,12 +33,10 @@ class HashMap:
         # address = self.get_address(id_num)
         # return self._buckets[address].name
 
-
     def has_this_address(self, id_num):
         address = self.get_address(id_num)
         return self._buckets[address] is not None
-    
-    
+
     def update_value(self, employee):
         print(f"Nome a ser atualizado: {self.get_value(employee.id_num)}")
         index = self.get_bucket_index(employee.id_num)
